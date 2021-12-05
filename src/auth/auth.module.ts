6488201e27517292590856fc085c3f9f74d22355
ammar <ammarraneez@gmail.com>
2021-12-05 20:34:31 +0530

    feat: jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
-  providers: [AuthService],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        // expire in 1h
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+
+  // Make the authorization implemented available for other modules
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,15 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
 import { UserRepository } from './users.repository';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private readonly userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   signUp(authCredentialsDto: AuthCredentialsDto) {
@@ -19,7 +22,11 @@ export class AuthService {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOne({ username });
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      const payload: JwtPayload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      // just a practice to return an object
+      return { accessToken };
     }
 
     // prevent saying something like 'user doesnt exist'
